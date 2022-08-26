47ee557f71ebf6b11515440719984216f642fa37
Giorgio <giorgio_mcd@live.it>
2022-08-26 18:12:08 +0200

    backup

---
+++ inf-master-thesis-inf-style-class/USIINFMScThesis/gym.py
@@ -0,0 +1,11 @@
+class GymTemplate(gym.Env):
+  def __init__(self):
+    pass
+  def step(action):
+    pass
+  def reset():
+    pass
+  def render(mode='human'):
+    pass
+  def close(self):
+    pass
